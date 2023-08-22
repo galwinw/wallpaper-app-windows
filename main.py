e90c14b7a973bf7db92bbb7d1fa1aa32e6128ad5
galwinw <gwillasgracias@gmail.com>
2023-08-22 12:25:03 -0400

    commit before exe changes

---
+++ main.py
@@ -0,0 +1,266 @@
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.text import LabelBase
+from wpchanger import WallpaperChanger
+import os
+import shutil
+from datetime import datetime
+import sys
+
+class WelcomeScreen(Screen):
+    def exit_app(self):
+        App.get_running_app().stop()
+        # if any random wallpapers are downloaded, delete them
+        path = os.path.dirname(os.path.abspath(__file__))
+        print('######################' + path)
+        for file in os.listdir(path):
+            if file.startswith('randomWallpaper') and file.endswith('.jpg'):
+                os.remove(os.path.join(path, file))
+
+class WallpaperScreen(Screen):
+    pass
+
+class RandomWallpaperScreen(Screen):
+    image = ObjectProperty(None)
+    path = os.path.dirname(os.path.abspath(__file__))
+    randomWallpapers = []
+    image_index = 0
+    with open(os.path.join(path, 'Saved\savedIndex.txt'), 'r') as f:
+        saved_index = int(f.read())
+    current_image_source = 'randomWallpaper1.jpg'  # Initially set to the first image
+
+    def on_pre_enter(self):
+        self.get_random_wallpapers()
+        self.download_random_wallpaper(True)
+
+    def get_random_wallpapers(self):
+        wallpaper_changer = WallpaperChanger()
+        self.randomWallpapers = wallpaper_changer.get_random_wallpapers(self.randomWallpapers)
+
+    def download_random_wallpaper(self, next):
+        if self.image_index == 26:
+            self.image_index = 0
+
+        wallpaper_changer = WallpaperChanger()
+        wallpaper_path = os.path.join(self.path, self.current_image_source)
+        #delete the previous image
+        if os.path.exists(wallpaper_path):
+            os.remove(wallpaper_path)
+
+        if next:
+            self.image_index = (self.image_index + 1) % len(self.randomWallpapers)
+        else:
+            self.image_index = (self.image_index - 1) % len(self.randomWallpapers)
+
+        self.current_image_source = 'randomWallpaper' + str(self.image_index) + '.jpg'
+        wallpaper_path = os.path.join(self.path, self.current_image_source)
+        wallpaper_changer.download_image(self.randomWallpapers[self.image_index], self.path, update=False, random=True, image_name= self.current_image_source)
+        self.update_image(wallpaper_path)
+        
+    def update_image(self, wallpaper_path):
+        print ("Setting image: " + wallpaper_path + " as wallpaper")
+        if os.path.exists(wallpaper_path):
+            self.image.source = wallpaper_path  # Set the image source again
+            print ("Image set as wallpaper")
+
+    def set_wallpaper(self):
+        wallpaper_changer = WallpaperChanger()
+        wallpaper_changer.set_wallpaper(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.current_image_source))
+        wallpaper_set()
+
+    def save_wallpaper(self):
+        # Ensure the 'saved' folder exists
+        saved_folder = os.path.join(self.path, 'Saved')
+        if not os.path.exists(saved_folder):
+            os.makedirs(saved_folder)
+
+        # Get the source filename
+        source_filename = os.path.basename(self.current_image_source)
+        
+        # Construct the destination filename
+        destination_filename = f"savedWallpaper{self.saved_index}.jpg"
+        destination_path = os.path.join(saved_folder, destination_filename)
+
+        try:
+            # Copy the file to the 'saved' folder with the new name
+            shutil.copyfile(self.current_image_source, destination_path)
+            print(f"Image '{source_filename}' saved as '{destination_filename}' in 'saved' folder.")
+            wallpaper_saved()
+            self.saved_index += 1
+            update_saved_index(self.saved_index)
+        except Exception as e:
+            print(f"Error saving image: {e}")
+
+class ViewImagesScreen(Screen):
+    image = ObjectProperty(None)
+    image_label = ObjectProperty(None)
+    
+    image_index = 0
+    path = os.path.dirname(os.path.abspath(__file__)) + "/wp-images/"
+    image_files = [file for file in os.listdir(path) if file.startswith('wallpaperOfTheDay') and file.endswith('.jpg')]
+    
+    
+    def on_pre_enter(self):
+        self.update_folder()
+        print(self.image_files)
+        self.update_image()
+
+    def update_folder(self):
+        self.image_files = [file for file in os.listdir(self.path) if file.startswith('wallpaperOfTheDay') and file.endswith('.jpg')]
+        if not self.image_files:
+            self.image_label.text = "No images in folder"
+            self.image.source = ""
+        # Sort the image files by date
+        self.image_files = sorted(self.image_files, key=lambda filename: self.extract_date(filename), reverse=True)
+
+    def extract_date(self, filename):
+        parts = filename.split(' ') 
+        if parts[3] == '':
+            parts.pop(3) 
+            parts[3] = '0' + parts[3] 
+        
+        date_string = ' '.join(parts[-4:]) 
+        date_string = date_string.replace('.jpg', '') 
+        return datetime.strptime(date_string, '%a %b %d %Y') 
+
+    def update_image(self):
+        if self.image_files:
+            image_file = self.image_files[self.image_index]
+            image_path = os.path.join(self.path, image_file)
+            self.image.source = image_path
+            self.image_label.text = image_file
+            
+    def next_image(self):
+        if self.image_files:
+            self.image_index = (self.image_index + 1) % len(self.image_files)
+            self.update_image()
+
+    def previous_image(self):
+        if self.image_files:
+            self.image_index = (self.image_index - 1) % len(self.image_files)
+            self.update_image()
+
+    def set_wallpaper(self):
+        wallpaper_changer = WallpaperChanger()
+        image_file = self.image_files[self.image_index]
+        wallpaper_changer.set_wallpaper(os.path.join(self.path, image_file))
+        wallpaper_set()
+
+    def delete_wallpaper(self):
+        if self.image_files:
+            image_file = self.image_files[self.image_index]
+            image_path = os.path.join(self.path, image_file)
+            os.remove(image_path)
+            self.image_files.remove(image_file)
+            self.image_index = (self.image_index - 1) % len(self.image_files)
+            wallpaper_deleted()
+            self.update_image()
+
+class SavedWallpaperScreen(Screen):
+    image = ObjectProperty(None)
+    image_label = ObjectProperty(None)
+    
+    image_index = 0
+
+    path = os.path.dirname(os.path.abspath(__file__)) + "/Saved/"
+    image_files = [file for file in os.listdir(path) if file.startswith('savedWallpaper') and file.endswith('.jpg')]
+
+    def on_pre_enter(self):
+        self.update_folder()
+        self.update_image()
+
+    def update_folder(self):
+        self.image_files = [file for file in os.listdir(self.path) if file.startswith('savedWallpaper') and file.endswith('.jpg')]
+        if not self.image_files:
+            self.image_label.text = "No saved wallpapers"
+            self.image.source = ""
+
+
+    def update_image(self):
+        if self.image_files:
+            image_file = self.image_files[self.image_index]
+            image_path = os.path.join(self.path, image_file)
+            self.image.source = image_path
+            self.image_label.text = image_file
+    
+    def next_image(self):
+        if self.image_files:
+            self.image_index = (self.image_index + 1) % len(self.image_files)
+            self.update_image()
+
+    def previous_image(self):
+        if self.image_files:
+            self.image_index = (self.image_index - 1) % len(self.image_files)
+            self.update_image()
+
+    def set_wallpaper(self):
+        wallpaper_changer = WallpaperChanger()
+        image_file = self.image_files[self.image_index]
+        wallpaper_changer.set_wallpaper(os.path.join(self.path, image_file))
+        wallpaper_set()
+    
+    def delete_wallpaper(self):
+        if self.image_files:
+            image_file = self.image_files[self.image_index]
+            image_path = os.path.join(self.path, image_file)
+            os.remove(image_path)
+            self.image_files.remove(image_file)
+            self.image_index = (self.image_index - 1) % len(self.image_files)
+            wallpaper_deleted()
+            self.update_image()
+
+class WindowManager(ScreenManager):
+    pass
+
+class InstructionScreen(Screen):
+    #path to wpchanger.py
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'wpchanger.py')
+
+def update_saved_index(index):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Saved\savedIndex.txt'), 'w') as f:
+        print("done")
+        f.write(str(index))
+
+def wallpaper_saved():
+    pop = Popup(title='Wallpaper Saved', content=Label(text='Wallpaper saved to the "Saved Wallpapers"'), size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+def wallpaper_set():
+    pop = Popup(title='Wallpaper Set', content=Label(text='Wallpaper set Succesfully'), size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+def wallpaper_deleted():
+    pop = Popup(title='Wallpaper Deleted', content=Label(text='Wallpaper deleted Succesfully'), size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        base_path = sys._MEIPASS
+
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+kv = Builder.load_file("my.kv")
+sm = WindowManager()
+
+screens = [WelcomeScreen(name="welcome"), WallpaperScreen(name="wallpaper"), ViewImagesScreen(name="viewimages"), RandomWallpaperScreen(name="random"), SavedWallpaperScreen(name="saved"), InstructionScreen(name="instructions")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "welcome"
+
+class WallpaperApp(App):
+    def build(self):
+        return sm
+
+if __name__ == "__main__":
+    LabelBase.register(name='MyFont', fn_regular= 'Fonts/MyFont3.ttf')
+    WallpaperApp().run()
--- wpchanger.py
+++ wpchanger.py
@@ -1,41 +1,67 @@
+#!/usr/bin/env python
 
 import time 
 import io
 import ctypes
-import http.client
 import praw
 import requests
-import urllib
-
+import os
+import random
 
 
 def main():
-    path = "C:/Users/galwi/redditWallpapers/"
-    dateToday = time.localtime()
+    path = os.path.dirname(os.path.abspath(__file__)) + "/wp-images/" 
     ddmmyy = time.ctime()[0:11] + time.ctime()[-1:-5:-1][::-1] # This is to get the date in dd/mm/yy format, ex: 01/01/16
     listOfDates = getDatesDownloaded(path) # This is a list of dates that have already been downloaded
     if(not((ddmmyy + "\n") in listOfDates)): 
-        pictureUrl = getImgurLink()    
-        downloadImage(pictureUrl , ddmmyy, path)
-        #setWallpaperForTheDay(ddmmyy, path)
+        pictureUrl = __getImage()    
+        downloadImage(pictureUrl, path, ddmmyy)
         set_wallpaper(path + "wallpaperOfTheDay " + ddmmyy + ".jpg")
     else:
         print("Already downloaded")
     
 
-def getImgurLink(): # This function returns the link to the top wallpaper of the day
+def __getImage(): # This function returns the link to the top wallpaper of the day
     redditObject = praw.Reddit(client_id = "T46sr4defGPqYMYBntnRUA", client_secret = "H8Snte7389b2Jee8mtJZi_usK52X4g",  user_agent = 'Daily Wallpaper Retriver for a new Windows Wallpaper')
 
     # Get the subreddit
-    subreddit = redditObject.get('r/wallpapers')
-
+    subreddit = redditObject.get('r/wallpaper')
     for post in subreddit:
         url = str(post.url)
 
         if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
             return url
             
+def __getImageRandom(): # This function returns the link to a random wallpaper
+    redditObject = praw.Reddit(client_id="T46sr4defGPqYMYBntnRUA", client_secret="H8Snte7389b2Jee8mtJZi_usK52X4g", user_agent='Daily Wallpaper Retriver for a new Windows Wallpaper')
+
+    # Get the subreddit
+    subreddit = redditObject.get('r/wallpaper')
+    posts = []
+    for post in subreddit:
+        posts.append(post)
+    random_post_number = random.randint(0, 10)
+    random_post = posts[random_post_number]
     
+    url = random_post.url
+    if url.endswith("jpg"):
+        print(url)
+        return url
+    else:
+        return __getImageRandom()
+    
+def getRandomWallpapers(arr): # This function returns a list of random wallpapers
+    redditObject = praw.Reddit(client_id="T46sr4defGPqYMYBntnRUA", client_secret="H8Snte7389b2Jee8mtJZi_usK52X4g", user_agent='Daily Wallpaper Retriver for a new Windows Wallpaper')
+
+    # Get the subreddit
+    subreddit = redditObject.get('r/wallpaper')
+    for post in subreddit:
+        url = str(post.url)
+        if url.endswith("jpg"):
+            arr.append(url)
+    return arr
+
+
 def getDatesDownloaded(path): # This function returns a list of dates that have already been downloaded
     logList = []
     logFile = open(path + "wallpaperLog.txt" , 'r')
@@ -43,25 +69,27 @@ def getDatesDownloaded(path): # This function returns a list of dates that have 
         logList.append(line)
     return logList
 
-def downloadImage(url , date, path): # This function downloads the image from the url and saves it in the path
+
+def downloadImage(url, path, date = '', update = True, random = False, image_name = ''): # This function downloads the image from the url and saves it in the path
     response = requests.get(url)
 
-    dataWriter = io.FileIO(path + 'wallpaperOfTheDay ' + date + '.jpg', 'w') # This is the file that the image data will be written to
+    if not random:
+        dataWriter = io.FileIO(path + 'wallpaperOfTheDay ' + date + '.jpg', 'w') # This is the file that the image data will be written to
+        print("Downloaded and added to list")
+    else:
+        dataWriter = io.FileIO(path + "\\" + image_name, 'w') # This is the file that the image data will be written to
+        print("Downloaded random wallpaper")
     dataWriter.write(response.content)
     dataWriter.close()
-    updateDateFile(date, path)
-    print("Downloaded and added to list")
+    if(update):
+        __updateDateFile(date, path)
 
     
-def updateDateFile(date, path): # This function updates the log file with the date of the downloaded image
+def __updateDateFile(date, path): # This function updates the log file with the date of the downloaded image
     logFile = open(path + 'wallpaperLog.txt','a')
     logFile.write(date + "\n")
     logFile.close()
     
-
-def setWallpaperForTheDay(date, path): # This function sets the wallpaper for the day
-    ctypes.windll.user32.SystemParametersInfoA(20,0,path + "wallpaperOfTheDay " + date + ".jpg", 0) # This is the function that sets the wallpaper
-
 def set_wallpaper(image_path):
     # Constants for the SPI_SETDESKWALLPAPER action and the FILE_ATTRIBUTE_NORMAL flag
     SPI_SETDESKWALLPAPER = 20
@@ -78,4 +106,21 @@ def set_wallpaper(image_path):
     else:
         print("Failed to set wallpaper.")
 
-main()
+class WallpaperChanger():
+    def start(self):
+        main()
+
+    def set_wallpaper(self, image_path):
+        set_wallpaper(image_path)
+
+    def get_random_wallpapers(self, arr):
+        return getRandomWallpapers(arr)
+    
+    def download_image(self, url, path, date='', update=True, random=False, image_name=''):
+        downloadImage(url, path, date, update, random, image_name)
+
+if __name__ == "__main__":
+    wc = WallpaperChanger()
+    wc.start()
+
+
