20bd3003ff1f0a977b012df87d6fa13a7917c9a5
galwinw <gwillasgracias@gmail.com>
2023-09-26 18:03:42 -0400

    update before resume

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import sys
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
+
     base_path = os.path.dirname(sys.executable)
     return os.path.join(base_path, relative_path)
     
